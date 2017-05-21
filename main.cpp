b3f6a09ac32f5824ca00eb3571554262383ad961
Simone Cimolato <Simonecimolato@users.noreply.github.com>
2017-05-21 16:54:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,18 +24,20 @@ int main(int argc, const char * argv[]) {
     int coprimes = 0;
     
     clock_t begin = clock();
-
+    
+    //generating random numbers and checking if they are coprime or not
     for (int i = 0; i <= total; i++){
         int first = rand();
         int second = rand();
         if (gcd(first, second) == 1) coprimes++;
     }
     
+    //calculating pi
     double pi = sqrt(6.0/(double)coprimes/(double)total);
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     
-    cout<<"Pi approx: "<<setprecision(20)<<pi<<endl     //another great approximation of pi is just 355/113, lol
+    cout<<"Pi approx: "<<setprecision(20)<<pi<<endl       //another great approximation of pi is just 355/113, lol
     <<"Precision: "<<fabs(100 - ((pi/3.14159265358979323846)*100))<<"%"<<endl<<endl;
     cout<<"Time spent: "<<setprecision(4)<<time_spent<<" seconds."<<endl<<endl;
 
